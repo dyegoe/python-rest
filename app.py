cb280403cf5da5d81c077cb758f90478be1d9d60
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2018-08-10 00:07:13 +0300

    add env support

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os import getenv
 from flask import Flask
 from flask_restful import Resource, Api
 from flaskext.mysql import MySQL
@@ -5,10 +6,10 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
 app = Flask(__name__)
 
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
-app.config['MYSQL_DATABASE_DB'] = 'dbtest'
-app.config['MYSQL_DATABASE_HOST'] = '172.17.0.3'
+app.config['MYSQL_DATABASE_USER'] = getenv('MYSQL_DATABASE_USER', 'root')
+app.config['MYSQL_DATABASE_PASSWORD'] = getenv('MYSQL_DATABASE_PASSWORD', 'password')
+app.config['MYSQL_DATABASE_DB'] = getenv('MYSQL_DATABASE_DB', 'db')
+app.config['MYSQL_DATABASE_HOST'] = getenv('MYSQL_DATABASE_HOST', 'localhost')
 
 
 mysql.init_app(app)
