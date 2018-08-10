a7e071b16993f1847f47509af93e0092a012eb44
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2018-08-11 01:41:39 +0300

    pritn info connection, change home message and change health to check database connection

--- src/app.py
+++ src/app.py
@@ -11,15 +11,32 @@ app.config['MYSQL_DATABASE_PASSWORD'] = getenv('MYSQL_DATABASE_PASSWORD', 'passw
 app.config['MYSQL_DATABASE_DB'] = getenv('MYSQL_DATABASE_DB', 'db')
 app.config['MYSQL_DATABASE_HOST'] = getenv('MYSQL_DATABASE_HOST', 'localhost')
 
+print("[INFO] MYSQL_DATABASE_USER = {0}".format(app.config['MYSQL_DATABASE_USER']))
+print("[INFO] MYSQL_DATABASE_PASSWORD = {0}".format(app.config['MYSQL_DATABASE_PASSWORD']))
+print("[INFO] MYSQL_DATABASE_DB = {0}".format(app.config['MYSQL_DATABASE_DB']))
+print("[INFO] MYSQL_DATABASE_HOST = {0}".format(app.config['MYSQL_DATABASE_HOST']))
+print("[INFO] HOSTNAME = {0}".format(getenv('HOSTNAME')))
 
 mysql.init_app(app)
 
 api = Api(app)
 
 
+class Home(Resource):
+    def get(self):
+        return {'Hostname': getenv('HOSTNAME')}, 200
+
+
 class Health(Resource):
     def get(self):
-        return {'StatusCode': '200', 'Status': 'Healthy', 'Hostname': getenv('HOSTNAME')}
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            conn.close()
+            return {'StatusCode': '200', 'Status': 'Healthy', 'Hostname': getenv('HOSTNAME')}, 200
+
+        except Exception as e:
+            return {'StatusCode': '500', 'error': str(e)}, 500
 
 
 class Users(Resource):
@@ -37,15 +54,15 @@ class Users(Resource):
                     'Age': item[1]
                 }
                 users.append(i)
-
-            return {'StatusCode': '200', 'Hostname': getenv('HOSTNAME'), 'Users': users}
+            conn.close()
+            return {'StatusCode': '200', 'Hostname': getenv('HOSTNAME'), 'Users': users}, 200
 
         except Exception as e:
-            return {'error': str(e)}
+            return {'StatusCode': '500', 'error': str(e)}, 500
 
 
-api.add_resource(Health, '/')
-
+api.add_resource(Home, '/')
+api.add_resource(Health, '/health')
 api.add_resource(Users, '/users')
 
 if __name__ == '__main__':
